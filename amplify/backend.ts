f169b828677763b78fa141ed909c777dbf47d765
Zack Saeger <zack.saeger@gmail.com>
2025-11-28 01:27:06 +0100

    Removed Default Auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
+//import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 defineBackend({
-  auth,
+  //auth,
   data,
 });
