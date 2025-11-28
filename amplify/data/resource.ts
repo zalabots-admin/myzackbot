51e06a327d6d60843a95238e7922f143c1ff28f5
Zack Saeger <zack.saeger@gmail.com>
2025-11-28 02:18:29 +0100

    ZackBot Data Model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,147 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
+
+import { type ClientSchema, a, defineData, defineFunction } from "@aws-amplify/backend";
+
+
 const schema = a.schema({
-  Todo: a
+  Organization: a
     .model({
-      content: a.string(),
+      Name: a.string().required(),
+      ContactFirstName: a.string(),
+      ContactLastName: a.string(),
+      ContactEmail: a.string(),
+      Address1: a.string(),
+      Address2: a.string(),
+      City: a.string(),
+      State: a.string(),
+      ZipCode: a.string(),
+      PrimaryColor: a.string(),
+      SecondaryColor: a.string(),
+      Logo: a.string(),
+      Requests: a.hasMany('Request', 'OrganizationID'),
+      Items: a.hasMany('Items', 'OrganizationID'),
+      Forms: a.hasMany('Forms', 'OrganizationID'),
+      Users: a.hasMany('Users', 'OrganizationID'),
+    }).authorization((allow) => [allow.publicApiKey()]),
+    Items: a
+    .model({
+      OrganizationID: a.string().required(),
+      Name: a.string().required(),
+      Type: a.string().required(),
+      Label: a.string().required(),
+      Description: a.string(),
+      Options: a.string(),
+      Layout: a.string(),
+      DocumentId: a.string(),
+      Organization: a.belongsTo('Organization', 'OrganizationID'),
+    }).authorization((allow) => [allow.publicApiKey()]),
+    Forms: a
+    .model({
+      OrganizationID: a.string().required(),
+      Name: a.string().required(),
+      Type: a.string().required(),
+      Description: a.string(),
+      FormItems: a.hasMany('FormItems', 'FormID'),
+      Organization: a.belongsTo('Organization', 'OrganizationID'),
+    }).authorization((allow) => [allow.publicApiKey()]),
+    FormItems: a
+    .model({
+      FormID: a.string().required(),
+      Name: a.string().required(),
+      Type: a.string().required(),
+      Label: a.string().required(),
+      Description: a.string(),
+      Options: a.string(),
+      Layout: a.string(),
+      DocumentId: a.string(),
+      Order: a.integer(),
+      Form: a.belongsTo('Forms', 'FormID'),
+    }).authorization((allow) => [allow.publicApiKey()]),
+    Request: a
+    .model({
+      OrganizationID: a.string().required(),
+      RequestedFor: a.string(),
+      AccountName: a.string(),
+      RequestType: a.string(),
+      EmailResponse: a.boolean(),
+      AutoComplete: a.boolean(),
+      DeliveryMethod: a.string(),
+      DueDate: a.string(),
+      RequestStatus: a.string(),
+      FollowUpDate: a.string(),
+      Organization: a.belongsTo('Organization', 'OrganizationID'),
+      History: a.hasMany('RequestHistory', 'RequestID'),
+      Participants: a.hasMany('RequestParticipants', 'RequestID'),
+      RequestTasks: a.hasMany('RequestTasks', 'RequestID'),
+      Questions: a.hasMany('RequestQuestions', 'RequestID'),
+    }).authorization(allow => [allow.publicApiKey()]),
+    RequestTasks: a
+    .model({
+      RequestID: a.string().required(),
+      RequestTaskStatus: a.string(),
+      Request: a.belongsTo('Request', 'RequestID'),
+      History: a.hasMany('RequestHistory', 'RequestTaskID'),
+      Participants: a.hasMany('RequestParticipants', 'RequestTaskID'),
+      Responses: a.hasMany('RequestResponses', 'RequestTaskID'),
+    }).authorization(allow => [allow.publicApiKey()]),
+    RequestParticipants: a
+    .model({
+      RequestID: a.string(),
+      RequestTaskID: a.string(),
+      FirstName: a.string(),
+      LastName: a.string(),
+      Email: a.string(),
+      ParticipantRole: a.string(),
+      Request: a.belongsTo('Request', 'RequestID'),
+      RequestTask: a.belongsTo('RequestTasks', 'RequestTaskID'),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .secondaryIndexes(index => [
+      index('ParticipantRole').sortKeys(['RequestID'])
+    ])
+    .authorization(allow => [allow.publicApiKey()]),
+    RequestQuestions: a
+    .model({
+      RequestID: a.string(),
+      Name: a.string(),
+      Type: a.string(),
+      Label: a.string(),
+      Description: a.string(),
+      Options: a.string(),
+      Layout: a.string(),
+      DocumentId: a.string(),
+      Order: a.integer(),
+      Request: a.belongsTo('Request', 'RequestID'),
+    }).authorization(allow => [allow.publicApiKey()]),
+    RequestResponses: a
+    .model({
+      RequestID: a.string(),
+      RequestTaskID: a.string(),
+      Name: a.string(),
+      Value: a.string(),
+      IsDocument: a.boolean(),
+      RequestTask: a.belongsTo('RequestTasks', 'RequestTaskID'),
+    }).authorization(allow => [allow.publicApiKey()]),
+    RequestHistory: a
+    .model({
+      RequestID: a.string(),
+      RequestTaskID: a.string(),
+      Event: a.string(),
+      Date: a.string(),
+      User: a.string(),
+      Description: a.string(),
+      Request: a.belongsTo('Request', 'RequestID'),
+      RequestTask: a.belongsTo('RequestTasks', 'RequestTaskID'),
+    }).authorization(allow => [allow.publicApiKey()]),
+    Users: a
+    .model({
+      OrganizationID: a.string().required(),
+      FirstName: a.string(),
+      LastName: a.string(),
+      Role: a.string(),
+      Active: a.boolean(),
+      Organization: a.belongsTo('Organization', 'OrganizationID'),
+    })  
+    .authorization(allow => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,38 +150,11 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
 });
 
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
 
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
